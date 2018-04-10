51dc943776fa5ed56ffd58f426e4a387e1eea5c0
SunMing <1229536801@qq.com>
2018-04-10 15:39:32 +0800

    完成practice10包里的Person、Student、Teacher和Klass类，并通过所有测试，实现了相应的README文件里所要求的功能

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,48 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+  private int number;
+  private Student leader;
+  private List<Student> members;
+
+  public Klass(int number) {
+    this.number = number;
+    members =new ArrayList<>();
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  public String getDisplayName(){
+    return "Class "+number;
+  }
+
+  public void assignLeader(Student student){
+    if (members.contains(student)){
+      leader = student;
+    }
+    else {
+      System.out.print("It is not one of us.\n");
+    }
+  }
+
+  public void appendMember(Student student) {
+    members.add(student);
+  }
+
+  public List<Student> getMembers() {
+    return members;
+  }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,93 @@
 package practice10;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Teacher extends Person{
+  private int id;
+  private String name;
+  private int age;
+  private Set<Klass> classes;
+
+  public Teacher() {
+    classes=new HashSet<>();
+  }
+
+  public Teacher(int id, String name, int age) {
+    super(id,name,age);
+    this.name = name;
+    this.age = age;
+    classes=new HashSet<>();
+  }
+
+  public Teacher(int id,String name, int age, Set<Klass> classes) {
+    super(id,name,age);
+    this.id=id;
+    this.name=name;
+    this.age=age;
+    this.classes=classes;
+    classes=new HashSet<>();
+  }
+
+  @Override
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Set<Klass> getClasses() {
+    return classes;
+  }
+
+  public void setClasses(Set<Klass> classes) {
+    this.classes = classes;
+  }
+
+  @Override
+  public String introduce() {
+    if(classes.size()==0){
+      return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+    else {
+      String klassNums="";
+      for(Klass klass:classes)
+        klassNums= klassNums+klass.getNumber()+", ";
+      klassNums=klassNums.substring(0,klassNums.length()-2);
+      return super.introduce()+" I am a Teacher. I teach Class "+klassNums+".";
+    }
+  }
+
+  public String introduceWith(Student student){
+    return super.introduce()+" I am a Teacher. I "+(isTeaching(student)?"":"don't ")+"teach "+student.getName()+".";
+  }
+
+  public boolean isTeaching(Student student){
+    for(Klass klass:classes){
+      if (klass==student.getKlass())
+        return true;
+    }
+    return false;
+  }
 }
+
