a7cc66c352aed37d6d9f9cc0bc19170cebcdd4d2
SunMing <1229536801@qq.com>
2018-04-10 15:16:04 +0800

    完成practice09包里的Person、Student、Teacher和Klass类，并通过所有测试，实现了相应的README文件里所要求的功能

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,44 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+  private int number;
+  private Student leader;
+  private List<Student> studentList;
+
+  public Klass(int number) {
+    this.number = number;
+    studentList=new ArrayList<>();
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  public String getDisplayName(){
+    return "Class "+number;
+  }
+
+  public void assignLeader(Student student){
+    if (studentList.contains(student)){
+      leader = student;
+    }
+    else {
+      System.out.print("It is not one of us.\n");
+    }
+  }
+
+  public void appendMember(Student student) {
+    studentList.add(student);
+  }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,58 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+  private int id;
+  private String name;
+  private int age;
+  private Klass klass;
+
+  public Student() {
+  }
+
+  public Student(int id,String name, int age, Klass klass) {
+    super(id,name,age);
+    this.id=id;
+    this.name=name;
+    this.age=age;
+    this.klass = klass;
+  }
+
+  @Override
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public void setKlass(Klass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String introduce() {
+    return super.introduce()+" I am a Student. I am "+(this==klass.getLeader()?"Leader of ":"at ")+klass.getDisplayName()+".";
+  }
 }
