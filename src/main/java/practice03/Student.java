41f4c8e7be5c5e741cb3164da1daf705fedfd165
SunMing <1229536801@qq.com>
2018-04-10 11:27:00 +0800

    完成practice03包里的Person、Student和Worker类，并通过所有测试，实现了相应的README文件里所要求的功能

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,46 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+  private String name;
+  private int age;
+  private int klass;
+
+  public Student() {
+  }
+
+  public Student(String name, int age, int klass) {
+    this.name = name;
+    this.age = age;
+    this.klass = klass;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getKlass() {
+    return klass;
+  }
+
+  public void setKlass(int klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String introduce() {
+    return "I am a Student. I am at Class "+klass+".";
+  }
 }
+
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,39 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+  private String name;
+  private int age;
+
+  public Worker() {
+  }
+
+  public Worker(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public int getAge() {
+    return age;
+  }
+
+  @Override
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  @Override
+  public String introduce() {
+    return "I am a Worker. I have a job.";
+  }
 }
