c301239ea945a36a5c9bc99b4ef50c2d55bdcc58
SunMing <1229536801@qq.com>
2018-04-10 14:56:30 +0800

    完成practice08包里的Person、Student、Teacher和Klass类，并通过所有测试，实现了相应的README文件里所要求的功能

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Klass {
+  private int number;
+  private Student leader;
+
+  public Klass(int number) {
+    this.number = number;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  public String getDisplayName(){
+    return "Class "+number;
+  }
+
+  public void assignLeader(Student student){
+    leader=student;
+  }
 }
+
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,68 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+  private int id;
+  private String name;
+  private int age;
+  private Klass klass;
+
+  public Teacher() {
+  }
+
+  public Teacher(int id, String name, int age) {
+    super(id,name,age);
+    this.name = name;
+    this.age = age;
+  }
+
+  public Teacher(int id,String name, int age, Klass klass) {
+    super(id,name,age);
+    this.id=id;
+    this.name=name;
+    this.age=age;
+    this.klass = klass;
+  }
+
+  @Override
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public void setKlass(Klass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String introduce() {
+    return super.introduce()+" I am a Teacher. I teach "+(klass!=null?klass.getDisplayName():"No Class")+".";
+  }
+
+  public String introduceWith(Student student){
+    return super.introduce()+" I am a Teacher. I "+(klass.getNumber()==student.getKlass().getNumber()?"":"don't ")+"teach "+student.getName()+".";
+  }
 }
