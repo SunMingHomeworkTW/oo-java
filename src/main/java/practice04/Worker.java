bb2306a27267f58e83fbe73bee12b26ef8f9dd80
SunMing <1229536801@qq.com>
2018-04-10 12:45:54 +0800

    完成practice04包里的Person、Student和Worker类，并通过所有测试，实现了相应的README文件里所要求的功能

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,41 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+  private String name;
+  private int age;
+
+  public Worker() {
+  }
+
+  public Worker(String name, int age) {
+    super(name,age);
+    this.name=name;
+    this.age=age;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public int getAge() {
+    return age;
+  }
+
+  @Override
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  @Override
+  public String introduce() {
+    return super.introduce()+" I am a Worker. I have a job.";
+  }
 }
+
