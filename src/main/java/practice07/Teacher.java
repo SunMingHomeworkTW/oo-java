223acc8d8370356116b022773215d96db2b16087
SunMing <1229536801@qq.com>
2018-04-10 13:27:14 +0800

    完成practice07包里的Person、Student、Teacher和Klass类，并通过所有测试，实现了相应的README文件里所要求的功能

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,57 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+  private String name;
+  private int age;
+  private Klass klass;
+
+  public Teacher() {
+  }
+
+  public Teacher(String name, int age) {
+    super(name,age);
+    this.name = name;
+    this.age = age;
+  }
+
+  public Teacher(String name, int age, Klass klass) {
+    super(name,age);
+    this.name=name;
+    this.age=age;
+    this.klass = klass;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public void setKlass(Klass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String introduce() {
+    return super.introduce()+" I am a Teacher. I teach "+(klass!=null?klass.getDisplayName():"No Class")+".";
+  }
+
+  public String introduceWith(Student student){
+    return super.introduce()+" I am a Teacher. I "+(klass.getNumber()==student.getKlass().getNumber()?"":"don't ")+"teach "+student.getName()+".";
+  }
 }
+
