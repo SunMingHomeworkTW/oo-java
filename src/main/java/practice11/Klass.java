241027743c33bcf03e865e93f3b2a50db2248efc
SunMing <1229536801@qq.com>
2018-04-10 22:16:55 +0800

    完成practice11中的Klass、Person、Student和Teacher类，通过了所有测试，实现了README文件中所要求的功能

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,66 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.List;
+import sun.reflect.Reflection;
+
 public class Klass {
+  private int number;
+  private Student leader;
+  private List<Student> members;
+  private Teacher teacher;
+
+  public Klass(int number) {
+    this.number = number;
+    members =new ArrayList<>();
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  public Teacher getTeacher() {
+    return teacher;
+  }
+
+  public void setTeacher(Teacher teacher) {
+    this.teacher = teacher;
+  }
+
+  public String getDisplayName(){
+    return "Class "+number;
+  }
+
+  public void assignLeader(Student student){
+    if (members.contains(student)){
+      leader = student;
+      if (teacher!=null)
+        System.out.print("I am "+ teacher.getName()+". I know "+student.getName()+" become Leader of "+getDisplayName()+".\n");
+    }
+    else {
+      System.out.print("It is not one of us.\n");
+    }
+  }
+
+  public void appendMember(Student student) {
+    members.add(student);
+    if(teacher!=null)
+      System.out.print("I am "+ teacher.getName()+". I know "+student.getName()+" has joined "+getDisplayName()+".\n");
+  }
+
+  public List<Student> getMembers() {
+    return members;
+  }
+
+  public boolean isIn(Student student){
+    return members.contains(student);
+  }
 }
